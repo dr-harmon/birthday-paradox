b83bcfd4d5ea7cb7b32735c8e049e637e0934149
Trevor Harmon <tharmon@laspositascollege.edu>
2024-01-25 18:06:46 -0800

    Add solution

--- test/birthday_test.cpp
+++ test/birthday_test.cpp
@@ -3,3 +3,28 @@
 #include "birthday.h"
 
 using namespace std;
+
+TEST_CASE("For a population of zero, nobody has the same birthday") {
+    Birthdays birthdays;
+    birthdays.fill(0);
+    REQUIRE_FALSE(hasSameBirthday(birthdays));
+}
+
+TEST_CASE("If everyone has a different birthday, nobody has the same birthday") {
+    Birthdays birthdays;
+    birthdays.fill(1);
+    REQUIRE_FALSE(hasSameBirthday(birthdays));
+}
+
+TEST_CASE("If there are only two people who share the same birthday, then somebody has the same birthday") {
+    Birthdays birthdays;
+    birthdays.fill(0);
+    birthdays[42] = 2;
+    REQUIRE(hasSameBirthday(birthdays));
+}
+
+TEST_CASE("If every birthday is shared by two people, then somebody has the same birthday") {
+    Birthdays birthdays;
+    birthdays.fill(2);
+    REQUIRE(hasSameBirthday(birthdays));
+}
